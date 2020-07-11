0e4019f6703fa0fabb80b62c68c885358438fed4
veronica lim <veron_yan@yahoo.com.sg>
2020-07-11 18:15:30 +0800

    created api.js

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // array of questions for user
-const questions = [
+const prompt = require('inquirer').createPromptModule()
+const fs = require('fs')
 
-];
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = (fileName, data) => {
+    fs.writeToFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
 }
 
 // function to initialize program
-function init() {
-
+const init = async _ => {
+    let rmObject = {}
+    do {
+        const {rmUser, rmRepo} = await prompt([
+        {
+            type: 'input',
+            name: 'rmUser',
+            message: 'Enter user name.'
+        },
+        {
+            type: 'input',
+            name: 'rmRepo',
+            message: 'Enter repository name.'
+        }
+        ])
+        rmObject = await
+    }
 }
 
 // function call to initialize program
