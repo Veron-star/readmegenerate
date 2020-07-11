175c4750ead3f2bc780e48ff454d8cee738f3f5b
veronica lim <veron_yan@yahoo.com.sg>
2020-07-11 09:46:46 +0800

    npm installed

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-function generateMarkdown(data) {
+const generateMarkdown = data => {
     return `
     <a href="${data.link}" style="float:right"><img src="${data.name}" title="${data.name}" width="120" height="120"></a>
     # ${data.title.toUpperCase()}
